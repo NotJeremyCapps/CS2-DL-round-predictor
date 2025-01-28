994b3a1a210bb98538ce2cde9e251c2a13b5db84
megant2321 <120533235+megant2321@users.noreply.github.com>
2025-01-27 19:54:06 -0800

    got tensor working, need to add padding and edit data processing

--- src/dataset.py
+++ src/dataset.py
@@ -1,46 +1,130 @@
 import os
 import torch
-import torchaudio
-import torchaudio.transforms as T
 from torch.utils.data import Dataset
+import torch.nn.functional as P
 from torch.utils.data import Subset
 import pandas as pd
-from sklearn.model_selection import train_test_split
+import csv
 
-
-def pad_sequence(batch):
-    # Make all tensor in a batch the same length by padding with zeros
-    batch = [item.t() for item in batch]
-    batch = torch.nn.utils.rnn.pad_sequence(batch, batch_first=True, padding_value=0.)
-    return batch.permute(0, 2, 1)
-
-def train_val_dataset(dataset, val_split=0.25):
-    train_idx, val_idx = train_test_split(list(range(len(dataset))), test_size=val_split)
-    datasets = {}
-    datasets['train'] = Subset(dataset, train_idx)
-    datasets['val'] = Subset(dataset, val_idx)
-    return datasets
 
 class CS2PredictionDataset(Dataset):
-    def __init__(self, data_root = f"../../game_demos"):
-        
-        self.csv_file_out = "preprocessed_data.csv"
-        self.data = []
-        self.labels = []
+    def __init__(self, list, sequence_length): #a data point is one tick/timestamp, target is win/loss, data is parameter data, sequence_length eg 30 seconds 
 
-        self.dataset = None
-
-        self.max_event_length = 0
-        
-        self.data_root = data_root
-
-
-        
-
-    def __getitem__(self, index):
-        data, label = self.data[index], self.labels[index]
+        #sets amount of data points/ticks for each sequence
+        self.sequence_length= sequence_length
        
+        #replace this section with section from get_item
+
+        self.list = list
+        #change into tensor
+        #self.data = torch.tensor(data)
+        #self.target = torch.tensor(target)
+
+      #reload
+        #read in csv
+        with open(self.list, 'r') as file1: #open text file to list of csv files
+            words = file1.read().strip().split()
+            print(words)
+            with open(words[0], 'r') as file2: #open first csv file as file2
+        #with open(self.list, 'r'):
+                readingcsv = csv.reader(file2)
+
+                self.all_data = []
+                for row in readingcsv: 
+                    self.all_data.append(row) #first index is row and then col
+
+        #create target array  
+        target = []
+        for i in range(1, len(self.all_data)-1):#increment over every row
+        #split into target and data indexes 
+            target.append(float(self.all_data[i][-4])) #create target array --------THIS SHOULD BE THE INDEX OF THE WIN!-----------
+
+        #create a tensor
+        self.target = torch.tensor(target)
+
+
+        #create data as float
+        int_data = []
+        for i in range(1, len(self.all_data)-1):#increment over every row
+            temp_array = []
+            for m in range(len(self.all_data[i])):
+                if(m == len(self.all_data[i]) - 4):# if increment is the result do not add
+                    continue
+                else:
+                    #convert to float
+                    if(self.all_data[i][m] != ''):
+                        temp_array.append(float(self.all_data[i][m])) 
+                    else:
+                        temp_array.append(0) #do not convert empty spaces, replace with 0 SHOULD FIX THIS IN ACTUAL CODE - EG SECONDARY WEAPON - THIS IS NOT A PERMANENT FIX ----
+
+            int_data.append(temp_array)
+
+
+
+
+        #change into tensor
+        self.data = torch.tensor(int_data) #data index 
+
+        print(self.data)
+        self.offset = 0 #offset from padding
+        self.csvfile = 1
+
+
+
+    
+
+
+    def __getitem__(self, index): #index is beginning index of sequence, this assumes all the data for rounds and games is sequential
+        
+        #--- We need a tick column --- 
+        index = index + self.offset
+        if self.data[index][0] == 0: #tick is 0 may need to pad
+            len_of_round = 0 #0 based
+            while(self.data[len_of_round + 1][0] > self.data[len_of_round][0]):
+                len_of_round += 1
+            excess= len_of_round%self.sequence_length # 0 based
+            if(excess!= 0): #round needs to be padded
+                x = P.pad(self.data[0:(self.sequence_length - excess-1)], (0, excess-1), value=0) 
+            #x = torch.cat((padding, self.data[index][0:len_of_round]), 0)
+        else: #if ticks restart -> pad
+           x = self.data[index :index + self.sequence_length]
+
+        '''
+        #reload
+        if(index + self.sequence_length == self.data.shape[0]):
+            #read in csv
+            readingcsv = csv.reader(self.list[self.csvfile])
+            self.all_data = []
+            for row in readingcsv: 
+                self.all_data.append(row) #first index is row and then col
+        
+            target = []
+            data = []
+            for i in range(len(self.all_data)):
+            #split into target and data indexes 
+                target.append(self.all_data[-3][i]) #create target array
+                del self.all_data[-3][i] #remove target from all data
+
+            #change into tensor
+            self.data = torch.tensor(self.all_data) #data index 
+            self.target = torch.tensor(target)
+
+            self.offset = 0 #offset from padding
+            self.csvfile +=1
+        '''
+        return x
+
+
+
+        data, label = self.tensor_dataset[index], self.labels[index]
+        #or self.tensor_dataset.columns will return labels of columns
+
+
+
         return data, label
     
-    def __len__(self):
-        return len(self.dataset)
+    def __len__(self): #samples in dataset
+        return self.data.shape[0]
+    
+
+
---
+++ src/test_dataset.py
@@ -0,0 +1,5 @@
+from dataset import CS2PredictionDataset
+
+
+
+CS2PredictionDataset('../game_demos/preprocessed/de_anubis/rounds.txt', 30)
