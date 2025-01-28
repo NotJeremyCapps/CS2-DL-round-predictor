6b2be314ed2a952f255ef4690c1aef4f7f7d014c
megant2321 <120533235+megant2321@users.noreply.github.com>
2025-01-28 15:00:46 -0800

    test_dataset update

--- src/test_dataset.py
+++ src/test_dataset.py
@@ -2,4 +2,6 @@ from dataset import CS2PredictionDataset
 
 
 
-CS2PredictionDataset('../game_demos/preprocessed/de_anubis/rounds.txt', 30)
+test = CS2PredictionDataset('../game_demos/preprocessed/de_anubis/rounds.txt', 30)
+test.__getitem__(0)
+test.__len__()
