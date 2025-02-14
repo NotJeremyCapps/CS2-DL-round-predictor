b4ee86c1aa9dc5d2587c731e6726ab8b56d83327
Douglas Crocker Jr <105105069+drcrockerjr@users.noreply.github.com>
2025-02-14 13:51:32 -0800

    training.py issues fixed

--- src/training.py
+++ src/training.py
@@ -30,6 +30,8 @@ class ModelTrainer():
 
         self.model = CS2LSTM(n_feature=None, out_feature=1,n_hidden=self.seq_len,n_layers=2)
 
+        self.hidden = self.model.init_hidden(self.batch_size)
+
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
@@ -62,7 +64,7 @@ class ModelTrainer():
                                      drop_last=True
                                      )
         
-        self.test_data = DataLoader(dataset=self.testset,
+        self.test_loader = DataLoader(dataset=self.testset,
                                      batch_size=self.batch_size,
                                      num_workers=0,
                                      drop_last=True
@@ -85,7 +87,7 @@ class ModelTrainer():
         correct = 0
         num_preds = 0
 
-        hidden = None
+        # hidden = None
         with tqdm(self.train_loader, unit="batch", leave=True) as tepoch:
             for batch_idx, (target, new_round, x_main_data, x_prim_weap, x_sec_weap) in enumerate(tepoch):
                 
@@ -101,7 +103,7 @@ class ModelTrainer():
                 #     print(f"Target: {target}, Shape: {target.size()};\n Main_data: {x_main_data}, Shape: {x_main_data.size()};\n Weapon_data: {x_prim_weap}, Shape: {x_prim_weap.size()};\n New_Round: {new_round}, Shape: {new_round.shape};\n\n\n", file = f)
         
 
-                out, hidden = self.model(x_main_data, x_prim_weap, x_sec_weap, hidden) #hidden is info from past
+                out, self.hidden = self.model(x_main_data, x_prim_weap, x_sec_weap, self.hidden) #hidden is info from past
 
                 out = out.squeeze() # Output comes out of self.model (batch_size, 1) for some reason
 
@@ -119,12 +121,12 @@ class ModelTrainer():
                 # Compute accuracy
                 correct += (binary_preds == target).int().sum()
 
-                accuracy = (binary_preds == target).float().mean()
+                accuracy = (binary_preds == target).float().mean() * 100
 
                 # if True in new_round:
                 #     hidden = self.model.init_hidden(self.batch_size) # reset hidden state on new round occurance TODO currently doesn reset everyround due to batching, prob need to do in model forward
                 # else: 
-                hidden = (hidden[0].detach(), hidden[1].detach()) # detach if maintaining to next batch
+                self.hidden = (self.hidden[0].detach(), self.hidden[1].detach()) # detach if maintaining to next batch
 
                 tepoch.set_postfix(loss=loss.item(), accuracy=accuracy, refresh=True)
 
@@ -141,10 +143,8 @@ class ModelTrainer():
 
     def test(self, epoch):
 
-        self.model.eval()
-
         # batches = len(self.testing_data)
-        hidden = self.model.hidden_init() #initialize hidden variable
+        # hidden = self.model.hidden_init() #initialize hidden variable
         self.model.eval()
         loss = 0
 
@@ -154,7 +154,7 @@ class ModelTrainer():
         num_preds = 0
 
         with torch.no_grad(): #stops gradient computation - it is unnecessary
-            with tqdm(self.train_loader, unit="batch", leave=True) as tepoch:
+            with tqdm(self.test_loader, unit="batch", leave=True) as tepoch:
                 for batch_idx, (target, new_round, x_main_data, x_prim_weap, x_sec_weap) in enumerate(tepoch):
                     
                     print(f"Len of main: {x_main_data.size(1)}") # get length of sequence dimension
@@ -169,7 +169,7 @@ class ModelTrainer():
                     #     print(f"Target: {target}, Shape: {target.size()};\n Main_data: {x_main_data}, Shape: {x_main_data.size()};\n Weapon_data: {x_prim_weap}, Shape: {x_prim_weap.size()};\n New_Round: {new_round}, Shape: {new_round.shape};\n\n\n", file = f)
             
 
-                    out, hidden = self.model(x_main_data, x_prim_weap, x_sec_weap, hidden) #hidden is info from past
+                    out, self.hidden = self.model(x_main_data, x_prim_weap, x_sec_weap, self.hidden) #hidden is info from past
 
                     out = out.squeeze() # Output comes out of self.model (batch_size, 1) for some reason
 
@@ -189,10 +189,10 @@ class ModelTrainer():
 
                     accuracy = (binary_preds == target).float().mean()
 
-                    if True in new_round:
-                        hidden = self.model.init_hidden(self.batch_size) # reset hidden state on new round occurance TODO currently doesn reset everyround due to batching, prob need to do in model forward
-                    else: 
-                        hidden = (hidden[0].detach(), hidden[1].detach()) # detach if maintaining to next batch
+                    # if True in new_round:
+                    #     hidden = self.model.init_hidden(self.batch_size) # reset hidden state on new round occurance TODO currently doesn reset everyround due to batching, prob need to do in model forward
+                    # else: 
+                    self.hidden = (self.hidden[0].detach(), self.hidden[1].detach()) # detach if maintaining to next batch
 
                     tepoch.set_postfix(loss=loss.item(), accuracy=accuracy, refresh=True)
 
@@ -214,7 +214,7 @@ class ModelTrainer():
 
         for epoch in range(self.total_epochs):
             self.train(epoch)
-
+            self.test(epoch)
 
 
 mt = ModelTrainer()
