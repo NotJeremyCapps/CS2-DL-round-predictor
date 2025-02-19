6255900ac3bfdec6dc264ad63cf16a6483f2bc85
NotJeremy <notjeremycapps@gmail.com>
2025-02-19 15:22:32 -0800

    opencv work
    committing to let megan see code

--- src/plot_demo.py
+++ src/plot_demo.py
@@ -3,6 +3,8 @@ from awpy import plot
 from player import Player
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from awpy.plot.utils import position_transform_axis
+import math
 #import sys
 
 #sys.path.append("/c/Users/notje/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0/LocalCache/local-packages/Python311/site-packages/opencv_cuda/install_script.py")
@@ -16,32 +18,28 @@ DEMO_NAME = "00nation-vs-eclot-m1-mirage.dem"
 
 OUTPUT_PATH = "../parsed_videos/"
 
+ASSETS_PATH = OUTPUT_PATH + "assets/"
+
 #fps = 60
 
-#frame_size = (1024, 1024)
+FRAME_SIZE = (1024, 1024)
+
+FPS = 60
+
+FOURCC = cv2.VideoWriter_fourcc(*'XVID')
 
 def main():
-    output_path = OUTPUT_PATH + "test.avi"
-    fps = 60
-    frame_size = (1024, 1024)
-    is_color = True
-    fourcc = cv2.VideoWriter_fourcc(*'XVID') # or 'MJPG', 'WMV1'
-
-    video_writer = cv2.VideoWriter(output_path, fourcc, fps, frame_size, is_color)
-
-    frame = np.zeros((frame_size[1], frame_size[0], 3), dtype=np.uint8) 
-    video_writer.write(frame)
 
     # Example: Read frames from folder
-    image_folder = '../parsed_videos/assets'
-    images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
-    for image in images:
-        frame = cv2.imread(os.path.join(image_folder, image))
-        cv2.circle(frame, (447,63), 8, (0,0,255), -1)
-        video_writer.write(frame)
+    #image_folder = '../parsed_videos/assets'
+    #images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+    #for image in images:
+        #frame = cv2.imread(os.path.join(image_folder, image))
+        #cv2.circle(frame, (int(translate_position(-1656, "x")), int(translate_position(-1800, "y"))), 8, (0,0,255), -1)
+        #video_writer.write(frame)
 
 
-    f = open("test.txt", "w")
+    #f = open("test.txt", "w")
 
 
     parser = Demo(PATH + DEMO_NAME)
@@ -59,11 +57,13 @@ def main():
     list_pos = []
     list_set = []
     dict_list = []
-    count = 0
+    #count = 0
 
     print(cv2.__file__)
 
     for round_num in range(len(parser.rounds)): #loops for every round played
+
+        print("ROUND " + str(round_num+1))
 
         first_tick_of_round = round_starts[round_num]
         start_tick_round_index = parser.ticks.query('tick == @first_tick_of_round').head(1).index[0] #query takes a long time and dont want to do it for every tick, index of data frame from index of tick 
@@ -72,6 +72,8 @@ def main():
         players:list[Player] = []
         for i in range(0,10):
             players.append(Player(name=f"player{i}", enums_path = "enums.json"))
+
+        video_writer = cv2.VideoWriter(OUTPUT_PATH+"Round_" + str(round_num+1) + ".avi", FOURCC, FPS, FRAME_SIZE, True)
             
         
         for y in range(len(range(round_starts[round_num], round_ends[round_num] + 1))): #loops for every tick in that round
@@ -87,6 +89,7 @@ def main():
             curr_tick_info = parser.ticks.loc[start_idx_curr_tick : start_idx_curr_tick+9] #gets 10 dataframes (1 for each player) for each tick
 
 
+
             if(curr_tick_info.empty):
                 for z in range(0, 10):
                     players[z].load_tick_data(prev_start_idx_curr_tick, prev_curr_tick_info, z)
@@ -98,19 +101,32 @@ def main():
                     prev_start_idx_curr_tick, prev_curr_tick_info = start_idx_curr_tick, curr_tick_info
                     #print(z, "TEST ", round_num)
 
-            for z in range(0, 1):
-                pos = (players[z].position[y][0],players[z].position[y][1],players[z].position[y][2])
-                print("TEAM: ", players[z].team_name)
-                print("NUM: ", players[z].player_name)
-                list_pos.append(pos)
-                list_set.append({"size":6.0, "color": (29/255,204/255,247/255) if(players[z].team_name == "CT") else (253/255,219/255,36/255)})
-            if(count%480==0):
-                dict_list.append({"points":list_pos, "point_settings":list_set})
-                f.write(str(players[z].position[y][0]) + str(players[z].position[y][1]) + str(players[z].position[y][2]) + "\n")
-            count += 1
-        #fig, ax = plot.plot(map_name, list_pos)
+            for z in range(0, 10):
+                if(math.isnan(players[z].position[y][0]) or math.isnan(players[z].position[y][1])):
+                    if(y!=0):
+                        players[z].position[y] = players[z].position[y-1]
+                    else:
+                        players[z].position[y] = (0,0,0)
+                    #players[z].load_tick_data(prev_start_idx_curr_tick, prev_curr_tick_info, z)
 
-        f.close
+            frame = cv2.imread(ASSETS_PATH+"de_mirage.png")
+            for z in range(0, 10):
+                pos = (players[z].position[y][0],players[z].position[y][1],players[z].position[y][2])
+                #frame = cv2.imread(ASSETS_PATH+"de_mirage.png")
+                cv2.circle(frame, (int(translate_position(pos[0], "x")), int(translate_position(pos[1], "y"))), 8, (0,0,255), -1)
+            video_writer.write(frame)
+                #print("TEAM: ", players[z].team_name)
+                #print("NUM: ", players[z].player_name)
+                #list_pos.append(pos)
+                #list_set.append({"size":6.0, "color": (29/255,204/255,247/255) if(players[z].team_name == "CT") else (253/255,219/255,36/255)})
+            #if(count%480==0):
+                #dict_list.append({"points":list_pos, "point_settings":list_set})
+                #f.write(str(players[z].position[y][0]) + str(players[z].position[y][1]) + str(players[z].position[y][2]) + "\n")
+            #count += 1
+        #fig, ax = plot.plot(map_name, list_pos)
+        video_writer.release()
+
+    #f.close()
     
 
     #settings = {
@@ -137,7 +153,7 @@ def main():
     #list.append(Tuple)
 
 
-    plot.gif(map_name=map_name, frames_data=dict_list, output_filename="game.gif", duration=4000)
+    #plot.gif(map_name=map_name, frames_data=dict_list, output_filename="game.gif", duration=4000)
 
     #fig, ax = plot.plot(map_name, list_pos)
 
@@ -148,6 +164,25 @@ def main():
 
     #def update(frame):
 
+
+MIRAGE_DATA ={
+        "pos_x": -3230,
+        "pos_y": 1713,
+        "scale": 5,
+}
+
+#position from awpy, axis being "x" or "y"
+def translate_position(position, axis):
+    axis = axis.lower()
+    if axis not in ["x", "y"]:
+        msg = f"'axis' has to be 'x' or 'y', not {axis}"
+        raise ValueError(msg)
+    start = MIRAGE_DATA["pos_" + axis]
+    scale = MIRAGE_DATA["scale"]
+
+    if axis == "x":
+        return (position - start) / scale
+    return (start - position) / scale
 
 
 main()
